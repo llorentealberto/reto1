e0bed444e34016417ad8d827614c53eb383dc49f
llorentealberto <llorentealberto@hotmail.com>
2023-10-18 15:54:37 +0200

    creacion de la funcion cuadrado

---
+++ proyecto1/index.js
@@ -0,0 +1,31 @@
+
+function suma(op1 ,op2){
+
+    console.log(op1+op2) ;
+}
+
+function resta(op1,op2){
+
+    console.log(op1-op2) ;
+}
+
+function div(op1, op2){
+
+    console.log( op1/op2);
+}
+function mult(op1 ,op2){
+
+    console.log( op1*op2);
+
+
+}
+
+function cuadrado(op1){
+
+    return op1*op1;
+
+
+}
+
+
+module.exports ={suma , resta , div , mult};
---
+++ proyecto1/run.js
@@ -0,0 +1,10 @@
+let myLib =require("./index");
+
+
+(myLib.suma(3,4));
+
+(myLib.resta(3,4));
+(myLib.mult(3,4));
+(myLib.div(3,4));
+
+
